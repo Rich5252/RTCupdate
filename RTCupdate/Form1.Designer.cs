71199b51139d6cfb8f3490dac96608bfdf27042d
Rich5252 <richard.lax7@gmail.com>
2026-03-03 20:38:42 +0000

    Add AppInstallerFiles. Activate hourly NTP clock update.

--- RTCupdate/Form1.Designer.cs
+++ RTCupdate/Form1.Designer.cs
@@ -254,6 +254,7 @@
             // 
             // timer1
             // 
+            timer1.Enabled = true;
             timer1.Interval = 3600000;
             timer1.Tick += btnNTPupdate_Click;
             // 
